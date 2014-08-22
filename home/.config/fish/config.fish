a0f4142c9604778557cf8f2e06b2ac87739c6cad
Nate Collings <nate@natecollings.com>
2014-08-21 22:11:04 -0700

    Added local fish config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,5 +3,6 @@ alias "tpOff"="~/source/git/disable-touchpad/disableTouchpad -d"
 alias "tpOn"="~/source/git/disable-touchpad/disableTouchpad -e"
 
 . "$HOME/.homesick/repos/homeshick/homeshick.fish"
+. "$HOME/.config/fish/local_config.fish"
 
 homeshick --quiet refresh
