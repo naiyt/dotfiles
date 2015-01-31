0b8446325c3921d9691dd59ef29b094e6677cf8c
Nate Collings <nate@natecollings.com>
2015-01-30 23:44:44 -0700

    fish time fix

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,7 @@
 set fish_greeting "You sorry fool. You could not be the chosen one."
 alias "tpOff"="~/source/git/disable-touchpad/disableTouchpad -d"
 alias "tpOn"="~/source/git/disable-touchpad/disableTouchpad -e"
+alias "time"="time -p"
 
 . "$HOME/.homesick/repos/homeshick/homeshick.fish"
 . "$HOME/.config/fish/local_config.fish"
