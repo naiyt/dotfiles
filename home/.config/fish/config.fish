9a1e069f27ffe7be306d35e471e23db27fdddf89
Nate Collings <nate@natecollings.com>
2015-07-12 18:23:56 -0600

    r3 and r4 fish aliases

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,8 @@ alias "time"="time -p"
 alias "be"="bundle exec $argv"
 alias "ber"="bundle exec rake $argv"
 alias "berr"="bundle exec rake rspec $argv"
+alias "r3"="rvm use 1.9.3"
+alias "r4"="rvm use 2.2.2"
 
 . "$HOME/.homesick/repos/homeshick/homeshick.fish"
 . "$HOME/.config/fish/local_config.fish"
