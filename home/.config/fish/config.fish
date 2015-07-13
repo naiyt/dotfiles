d193af0478b2ae63de5c76ca0fb4c0b0995a867f
Nate Collings <nate@natecollings.com>
2015-07-12 18:26:16 -0600

    get rid of broken alias

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,7 +1,6 @@
 alias "time"="time -p"
 alias "be"="bundle exec $argv"
 alias "ber"="bundle exec rake $argv"
-alias "berr"="bundle exec rake rspec $argv"
 alias "r3"="rvm use 1.9.3"
 alias "r4"="rvm use 2.2.2"
 
